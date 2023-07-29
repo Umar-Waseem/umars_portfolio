a758a1c9f2789e9b76f3b706cc5ead22fdd88f28
Umar-Waseem <umar.waseem@gmail.com>
2023-07-30 00:07:51 +0500

    padding adjusted for smaller screen

--- src/sections/AboutSection.tsx
+++ src/sections/AboutSection.tsx
@@ -23,7 +23,7 @@ export default function AboutSection() {
 
 
   return (
-    <main className='about-section flex flex-col justify-center h-screen p-20 bg-black lg:items-start items-center text-white theContainer section'>
+    <main className='about-section flex flex-col justify-center h-screen p-10 bg-black lg:items-start items-center text-white theContainer section'>
       <h2 className='font-semibold text-5xl lg:text-7xl mb-4 lg:text-left text-center'>Muhammad Umar Waseem</h2>
       <p className='font-semibold text-3xl lg:text-4xl mb-6 gradient-text lg:text-left text-center'>
         Building Software Solutions That Hold Value
